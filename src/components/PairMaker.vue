f5c0d3fbcc3488b969a9e4f8ff3c4fe919593015
Felipe Flor <hello@felipeflor.com>
2022-07-21 02:51:50 -0400

    Adjust styling

--- src/components/PairMaker.vue
+++ src/components/PairMaker.vue
@@ -44,31 +44,39 @@ function saveProposedPairings() {
       <button id="add-name" :disabled="newName.trim().length === 0" @click="saveNewName">Save Name</button>
     </form>
 
-    <ul id="all-names-list">
+    <div id="all-names-list">
       <h2>All Names</h2>
-      <li v-for="name in names" :key="name">
-        <p>{{ name }}</p>
-        <button class="delete-name" @click="() => deleteName(name)">X</button>
-      </li>
-    </ul>
+      <ul>
+        <li v-for="name in names" :key="name">
+          <p>{{ name }}</p>
+          <button class="delete-name" @click="() => deleteName(name)">X</button>
+        </li>
+      </ul>
+    </div>
 
-    <ul id="pairs-list">
+    <div id="pairs-list">
       <h2>Proposed Pairs</h2>
       <button id="see-proposed-pairings" @click="displayProposedPairings">Propose pairs</button>
       <button id="save-proposed-pairing" @click="saveProposedPairings">Save pairs</button>
-      <li v-for="proposedPairing in proposedPairingList" :key="proposedPairing">{{ proposedPairing }}</li>
-    </ul>
+      <ul>
+        <li v-for="proposedPairing in proposedPairingList" :key="proposedPairing">{{ proposedPairing }}</li>
+      </ul>
+    </div>
 
-    <ul id="pairing-history">
+    <div id="pairing-history">
       <h2>Pairing History</h2>
-      <li v-for="pairingHistoryEntry in pairingHistoryList" :key="pairingHistoryEntry">{{ pairingHistoryEntry }}</li>
-    </ul>
+      <ul>
+        <li v-for="pairingHistoryEntry in pairingHistoryList" :key="pairingHistoryEntry">{{ pairingHistoryEntry }}</li>
+      </ul>
+    </div>
+
   </section>
 </template>
 
 <style scoped>
 section {
   display: grid;
+  grid-gap: 3rem;
   grid-template-areas:
         "input input input"
         "names pairs history"
